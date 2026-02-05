89ab50cec885672415596481aa809456d26eba74
Mukhamed Sattarov <mukhamed.sattarov@ftel.kz>
2026-02-05 17:33:39 +0500

    js2

--- Landing/js/main.js
+++ Landing/js/main.js
@@ -1,2 +1,8 @@
 'use strict'
-
+// animation
+var typed = new Typed(".typing", {
+    strings:["Frontender with JavaScript", "It Manager", "Spider Man"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
--- Landing/js/style-switcher.js
+++ Landing/js/style-switcher.js
@@ -36,3 +36,30 @@ function setActiveStyle(color)
 //         style.disabled = style.getAttribute("title") !== color;
 //     });
 // }
+
+// document.querySelectorAll('.colors span').forEach(span => {
+//     span.addEventListener('click', () => {
+//         const color = span.classList[0]; // "color-3"
+//         setActiveStyle(color);
+//     });
+// });
+
+// day and night
+
+const dayNight = document.querySelector(".day-night");
+dayNight.addEventListener("click", () => {
+    dayNight.querySelector("i").classList.toggle("fa-sun");
+    dayNight.querySelector("i").classList.toggle("fa-moon");
+    document.body.classList.toggle("dark");
+})
+
+window.addEventListener("load", () => {
+    if(document.body.classList.contains("dark"))
+    {
+        dayNight.querySelector("i").classList.add("fa-moon");
+    }
+    else
+    {
+        dayNight.querySelector("i").classList.add("fa-sun");
+    }
+})
